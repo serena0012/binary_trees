fcafc4497eb164f1e9e34f4b51709ee95ff0a0c1
Gbenga Elegbede <jonathangnenga@gmail.com>
2023-03-03 01:53:48 +0100

    Updated

--- binary_trees.h
+++ binary_trees.h
@@ -82,6 +82,14 @@ heap_t *heap_insert(heap_t **root, int value);
 heap_t *array_to_heap(int *array, size_t size);
 int heap_extract(heap_t **root);
 int *heap_to_sorted_array(heap_t *heap, size_t *size);
+void binary_tree_print(const binary_tree_t *);
+
+avl_t *rebalance(avl_t *node, avl_t **tree);
+avl_t *balance_left(avl_t *node);
+avl_t *balance_right(avl_t *node);
+char *convert(unsigned long int num, int base, int lowercase);
+bst_t *swap(bst_t *a, bst_t *b);
+
+size_t _binary_tree_height(const binary_tree_t *tree);
 
 #endif /* BINARY_TREES_H */
-
